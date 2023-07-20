78efffc35649fb4c51ca2943f77bf0f7d354170c
jmsmith23 <smitjeff23@gmail.com>
2023-07-19 22:53:24 -0500

    Finish shopping list app and check all functionality to ensure it works as intended

--- script.js
+++ script.js
@@ -21,6 +21,23 @@ function onAddItemSubmit(e) {
     alert('Please Add An Item');
     return;
   }
+
+  // Check for edit mode
+
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('.edit-mode');
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove('edit-mode');
+    itemToEdit.remove();
+    isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('That item already exists!');
+      return;
+    }
+  }
+
   // Create item DOM element
   addItemToDOM(newItem);
   //Add item to local storage
@@ -86,6 +103,11 @@ function onClickItem(e) {
   }
 }
 
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
+}
+
 function setItemToEdit(item) {
   isEditMode = true;
   itemList
@@ -143,6 +165,7 @@ function filterItems(e) {
 }
 
 function checkUI() {
+  itemInput.value = '';
   const items = itemList.querySelectorAll('li');
   console.log(items);
   if (items.length === 0) {
@@ -152,6 +175,10 @@ function checkUI() {
     clearButton.style.display = 'block';
     itemFilter.style.display = 'block';
   }
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = '#333';
+
+  isEditMode = false;
 }
 
 //Initialize App
